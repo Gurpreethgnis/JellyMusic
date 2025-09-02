e36f9bfdfe8596f67b261f42a2e856f25a1e15bc
Gurpreet Singh <gurpreets.nanda@gmail.com>
2025-09-01 22:57:59 -0400

    Fix JDK image transformation error in Android Studio - Project now builds successfully

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -15,6 +15,11 @@ android {
         minSdk = 24
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
 
     buildTypes {
         release {
