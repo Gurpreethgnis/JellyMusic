0759ea584a3026b6ef2dbeda3519207c5fc6a67d
Gurpreet Singh <gurpreets.nanda@gmail.com>
2025-09-01 23:02:06 -0400

    Clean up deprecated Gradle options and fix duplicate compileOptions

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -31,11 +31,6 @@ android {
         }
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
     kotlinOptions {
         jvmTarget = "17"
     }
